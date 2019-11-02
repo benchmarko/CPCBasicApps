019451c746eb3fb50cd11b86c353ac3908be792b
Benchmarko <benchmarko@users.noreply.github.com>
2019-11-02 20:19:19 +0100

    scpc6128, spacewar, stardodg

---
+++ apps/0index.js
@@ -0,0 +1,21 @@
+/* 0index.js - index file for BASIC examples */
+/* globals cpcBasic */
+
+"use strict";
+
+cpcBasic.addIndex("apps", function () { /*
+[
+    {
+    	"key": "demo/scpc6128",
+		"title": "Schneider CPC 6128 Demo"
+    },
+	{
+		"key": "games/spacewar",
+		"title": "Spacewar Game (Textual)"
+	},
+	{
+		"key": "games/stardodg",
+		"title": "Stardodger Game (Textual)"
+	}
+]
+*/ });
